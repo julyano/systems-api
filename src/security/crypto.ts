d590ddd626c0c97a29c96c7c28f9a96f8721477d
Juliano Cardoso <cardoso.juliano@gmail.com>
2021-03-06 01:14:17 -0300

    Implementação do método check() para comparar senhas Relacionada #29

--- src/security/crypto.ts
+++ src/security/crypto.ts
@@ -1,8 +1,12 @@
 import * as bcrypt from 'bcrypt';
 
 export class Crypto {
-  public static async encrypt(plainText): Promise<string> {
+  public static async encrypt(plainText: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(plainText, salt);
+    return await bcrypt.hash(plainText.toString(), salt);
+  }
+
+  public static async check(plainText: string, hashText: string) {
+    return await bcrypt.compare(plainText.toString(), hashText.toString());
   }
 }
