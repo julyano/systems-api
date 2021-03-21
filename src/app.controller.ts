582ff105719924d2a7f1f90d52722d3cbeecdd36
Juliano Cardoso <cardoso.juliano@gmail.com>
2021-03-21 00:46:22 -0300

    Adição do tipo any nas requisições do app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,13 +18,13 @@ export class AppController {
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
+  async login(@Request() req: any) {
     return this.authService.login(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
+  getProfile(@Request() req: any) {
     return req.user;
   }
 }
